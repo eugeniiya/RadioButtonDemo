88059daf76b1d0b64a6b3b11bc92db5add563539
user <user@ST3-U455-02R>
2021-10-20 17:40:28 +0300

    Добавьте файлы проекта.

---
+++ RadioButtonDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RadioButtonDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new string[] { "RadioButton_Experimental" });
+            MainPage = new MainPage();
+        }
+    }
+}
